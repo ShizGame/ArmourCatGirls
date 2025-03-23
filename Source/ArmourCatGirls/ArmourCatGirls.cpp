6cb0a71f63c391560407adaf546d1eba5333c57b
Viktor Salichkin <viktor.salichkin@guestmg.pro>
2025-03-23 18:45:46 +0400

    Init commit 2

--- Source/ArmourCatGirls/ArmourCatGirls.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ArmourCatGirls.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArmourCatGirls, "ArmourCatGirls" );
